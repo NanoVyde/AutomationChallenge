45d840aae63a4b0c153b5ab75e5eb4e646b37b13
NanoVyde <alvydev@gmail.com>
2022-02-28 11:42:20 +0200

    6 test added

--- cypress/integration/AutomationChallenge/6-DropDownChallenge.spec.js
+++ cypress/integration/AutomationChallenge/6-DropDownChallenge.spec.js
@@ -9,20 +9,30 @@ describe("Drop Down Challenge", () => {
   });
 
   it("1 - Verify Error Message and Country when selected country is NOT Lithuania! ", () => {
-    cy.get("#country").should("have.value", "Afganistan");
     cy.get("#country").select("Ukraine");
     cy.get("#dropdown-verify-btn").click();
     cy.confirmMessage("Selected country is Ukraine, NOT Lithuania!");
   });
-  //#country > option:nth-child(29)
 
-  // it("2 - Verify Error Message with at least 3 countries", () => {});
+  it("2 - Verify Error Message with at least 3 countries", () => {
+    cy.get("#country").select("Germany");
+    cy.get("#dropdown-verify-btn").click();
+    cy.confirmMessage("Selected country is Germany, NOT Lithuania!");
 
-  // it("3 - To solve a challenge select the country Lithuania from drop down list and VERIFY it!", () => {
-  //   cy.get("#country > option:nth-child(125)").click({force: true});
-  //   cy.get("#dropdown-verify-btn").click({force: true});
-  //   cy.url(
-  //     "https://software-testers.gitlab.io/challenges/automation-challenges/success.html"
-  //   );
-  // });
+    cy.get("#country").select("Italy");
+    cy.get("#dropdown-verify-btn").click();
+    cy.confirmMessage("Selected country is Italy, NOT Lithuania!");
+
+    cy.get("#country").select("Norway");
+    cy.get("#dropdown-verify-btn").click();
+    cy.confirmMessage("Selected country is Norway, NOT Lithuania!");
+  });
+
+  it("3 - To solve a challenge select the country Lithuania from drop down list and VERIFY it!", () => {
+    cy.get("#country").select("Lithuania");
+    cy.get("#dropdown-verify-btn").click({ force: true });
+    cy.url(
+      "https://software-testers.gitlab.io/challenges/automation-challenges/success.html"
+    );
+  });
 });
